d240cb390d31ca982d96a54d7fae8649a092aa49
kratosB <www12345678-007@163.com>
2017-08-03 18:35:27 +0800

    Create ItemService.java

---
+++ api/src/main/java/com/service/ItemService.java
@@ -0,0 +1,33 @@
+package com.service;
+
+import com.bean.heroitem.ItemsEntity;
+import com.util.JsonMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created on 2017/06/14.
+ */
+@Service
+public class ItemService {
+
+    private String steamUrl = "http://api.steampowered.com/IEconDOTA2_570/";
+
+    private String version = "v1/";
+
+    private String key = "?key=EFA1E81676FCC47157EA871A67741EF5";
+
+    private String str2 = "&";
+
+    private String language = "language=zh";
+
+    public ItemsEntity listAll() {
+        String getItem = "GetGameItems/";
+        String getHeroUrl = steamUrl + getItem + version + key + str2 + language;
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(getHeroUrl, String.class);
+        ItemsEntity entity = JsonMapper.nonDefaultMapper().fromJson(response, ItemsEntity.class);
+        return entity;
+    }
+
+}
