6a1d6273030ef9d52a32748cea244f272d50a210
admin <zhiqiang.bao@iqunxing.com>
2018-05-23 18:11:41 +0800

    exception handler

---
+++ api/src/main/java/com/util/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.util;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created on 2018/5/23.
+ * @author zhiqiang bao
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public String processException(Exception e){
+        System.out.println(e.getLocalizedMessage());
+        System.out.println(e.getStackTrace()[0]);
+        System.out.println(e.getStackTrace()[1]);
+        System.out.println(JsonMapper.nonDefaultMapper().toJson(e));
+        System.out.println(e.getMessage());
+        return e.getMessage();
+    }
+
+}
