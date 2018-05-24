4586f7b346e7a8612f26d6af32a18902ea1c733d
admin <zhiqiang.bao@iqunxing.com>
2018-05-24 17:44:28 +0800

    comment

--- api/src/main/java/com/dao/entity/MatchHistory.java
+++ api/src/main/java/com/dao/entity/MatchHistory.java
@@ -55,6 +55,30 @@ public class MatchHistory {
 
     private int negativeVotes;
 
+    /**
+     * game_mode
+     0 - None
+     1 - All Pick
+     2 - Captain's Mode
+     3 - Random Draft
+     4 - Single Draft
+     5 - All Random
+     6 - Intro
+     7 - Diretide
+     8 - Reverse Captain's Mode
+     9 - The Greeviling
+     10 - Tutorial
+     11 - Mid Only
+     12 - Least Played
+     13 - New Player Pool
+     14 - Compendium Matchmaking
+     15 - Co-op vs Bots
+     16 - Captains Draft
+     18 - Ability Draft
+     20 - All Random Deathmatch
+     21 - 1v1 Mid Only
+     22 - Ranked Matchmaking
+     */
     private int gameMode;
 
     private int flags;
